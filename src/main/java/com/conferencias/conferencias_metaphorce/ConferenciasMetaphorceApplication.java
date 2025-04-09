7104838b630b991aac1a91fbdedde9a8a19b9c4c
Roberto Torres <tovi.rob20@gmail.com>
2025-04-09 00:53:04 -0600

    testeando endpoint para obtener lista de participantes

--- src/main/java/com/conferencias/conferencias_metaphorce/ConferenciasMetaphorceApplication.java
+++ src/main/java/com/conferencias/conferencias_metaphorce/ConferenciasMetaphorceApplication.java
@@ -1,20 +1,11 @@
 package com.conferencias.conferencias_metaphorce;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.conferencias.conferencias_metaphorce.models.Participante;
-import com.conferencias.conferencias_metaphorce.repositories.ParticipanteRepository;
-
 @SpringBootApplication
 public class ConferenciasMetaphorceApplication implements CommandLineRunner {
-
-	@Autowired
-	private ParticipanteRepository participanteRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConferenciasMetaphorceApplication.class, args);
